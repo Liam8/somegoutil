9fd529692a1f35f00c6db2d1f5674d18f4ad1325
Liam <liam.su8@gmail.com>
2023-11-01 15:25:08 +0800

    add RandSet

---
+++ random_access_set.go
@@ -0,0 +1,52 @@
+package somegoutil
+
+import "math/rand"
+
+type RandSet[T comparable] struct {
+	arr []T
+	idxMap map[T]int
+}
+
+func NewRandSet[T comparable]() *RandSet[T] {
+	return &RandSet[T]{
+		arr: []T{},
+		idxMap: map[T]int{},
+	}
+}
+
+func (r *RandSet[T]) Add(val T) {
+	_, ok := r.idxMap[val]
+	if ok {
+		return
+	}
+
+	r.arr = append(r.arr, val)
+	r.idxMap[val] = len(r.arr) - 1
+}
+
+func (r *RandSet[T]) Remove(val T) {
+	idx, ok := r.idxMap[val]
+	if !ok {
+		return
+	}
+
+	lastIdx := len(r.arr)-1
+	r.arr[idx] = r.arr[lastIdx]
+	r.idxMap[r.arr[idx]] = idx
+
+	r.arr = r.arr[:len(r.arr) - 1]
+	delete(r.idxMap, val)
+}
+
+func (r *RandSet[T]) RandomPick() T {
+	if len(r.arr) > 0 {
+		return r.arr[rand.Intn(len(r.arr))]
+	} else {
+		return *new(T)
+	}
+}
+
+func (r *RandSet[T]) Exists(val T) bool {
+	_, ok := r.idxMap[val]
+	return ok
+}
---
+++ random_access_set_test.go
@@ -0,0 +1,75 @@
+package somegoutil
+
+import (
+	"testing"
+)
+
+func TestAdd(t *testing.T) {
+	tests := []struct {
+		name string
+		args int
+		want bool
+	}{
+		{
+			name: "add number1",
+			args: 123,
+			want: true,
+		},
+		{
+			name: "add number2",
+			args: 456,
+			want: true,
+		},
+	}
+	set := NewRandSet[int]()
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			set.Add(tt.args)
+			if got := set.Exists(tt.args); got != tt.want {
+				t.Errorf("Exists() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestRemove(t *testing.T) {
+	tests := []struct {
+		name string
+		args int
+		want bool
+	}{
+		{
+			name: "remove number1",
+			args: 123,
+			want: false,
+		},
+		{
+			name: "remove number2",
+			args: 456,
+			want: false,
+		},
+	}
+	set := NewRandSet[int]()
+	set.Add(123)
+	set.Add(456)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			set.Remove(tt.args)
+			if got := set.Exists(tt.args); got != tt.want {
+				t.Errorf("Exists() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestRandomPick(t *testing.T) {
+	set := NewRandSet[int]()
+	set.Add(123)
+	set.Add(456)
+	for i := 10; i>0; i-- {
+		v := set.RandomPick()
+		if v!=123 && v!=456 {
+			t.Errorf("RandomPick() returned unpected value = %v", v)
+		}
+	}
+}
